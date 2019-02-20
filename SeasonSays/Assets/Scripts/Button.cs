77a96eb4fa66bcdff644ef9106eb2fdeaa933753
Emma Lisowski <elisowsk@uci.edu>
2019-02-19 21:48:38 -0800

    Emma's Scene Added + Simon Logic and Gameplay

---
+++ SeasonSays/Assets/Scripts/Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonUnityEvent: UnityEvent<Button> { };
+
+public class Button : MonoBehaviour
+{
+    private Material material;
+    private Color color;
+    public ButtonUnityEvent buttonHit = new ButtonUnityEvent();
+
+    
+    void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+        color = material.color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player")) 
+        {
+            buttonHit.Invoke(this);
+        }
+    }
+
+    public IEnumerator lightUp()
+    {
+        material.color = Color.white;
+
+        yield return new WaitForSeconds(1);
+
+        material.color = color;
+    }
+}
---
+++ SeasonSays/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public List<GameObject> Seasons;
+
+    private int patternLength = 1;
+
+    private int currentButton = 0;
+
+    private List<int> pattern = new List<int>();
+
+    private List<string> seasons = new List<string>();
+
+    private bool patternStart = true;
+
+    void Awake()
+    {
+        pattern.Add(Random.Range(0, 4));
+        
+        seasons.Add("Spring");
+        seasons.Add("Summer");
+        seasons.Add("Fall");
+        seasons.Add("Winter");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject season in Seasons)
+        {
+            Button b;
+
+            b = season.GetComponent<Button>();
+
+            b.buttonHit.AddListener(completeListener);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (patternStart)
+        {
+            playPattern();
+        }
+    }
+
+    void playPattern() 
+    {
+        patternStart = false;
+
+        StartCoroutine(play());
+    }
+
+    IEnumerator play()
+    {
+        foreach (int p in pattern)
+        {
+            Debug.Log(p);
+            Debug.Log(seasons[p]);
+
+            yield return new WaitForSeconds(2);
+            
+            GameObject currentButton = GameObject.FindGameObjectWithTag(seasons[p]);
+
+            StartCoroutine(currentButton.GetComponent<Button>().lightUp());
+
+        }
+    }
+
+
+
+    void completeListener(Button b)
+    {
+        if (b.tag == seasons[pattern[currentButton]] && currentButton + 1 != patternLength)
+        {
+            currentButton += 1;
+        }
+
+        else if (b.tag != seasons[pattern[currentButton]])
+        {
+            Application.Quit();
+            //trigger some kind of game over screen
+        }
+
+        else if (b.tag == seasons[pattern[currentButton]] && currentButton + 1 == patternLength)
+        {
+            addPattern();
+            currentButton = 0;
+        }
+        
+    }
+
+    void addPattern()
+    {
+        pattern.Add(Random.Range(0,4));
+        patternLength += 1;
+        patternStart = true;
+    }
+}
