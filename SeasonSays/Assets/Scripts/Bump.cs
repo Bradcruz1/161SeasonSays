13332fa5396a51dc43bc5965b301a6f694ddf4db
Brad <brad.dela.cruz@icloud.com>
2019-02-26 18:43:37 -0800

    main menu stuff

--- SeasonSays/Assets/Scripts/Bump.cs
+++ SeasonSays/Assets/Scripts/Bump.cs
@@ -7,7 +7,7 @@ public class Bump : MonoBehaviour
     // Start is called before the first frame update
     void OnCollisionEnter(Collision other)
     {
-        float force = 5000;
+        float force = 200;
         if (other.gameObject.CompareTag("Fire"))
         {
             Vector3 dir = other.contacts[0].point - transform.position;
---
+++ SeasonSays/Assets/Scripts/Move1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move1 : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public float jump = 5;
+    private bool onGround = true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        move();
+        Jump();
+
+     }
+    void move()
+    {
+        float hori = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hori, 0f, verti);
+
+        rb.AddForce(movement * speed);
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown("space") && onGround)
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            onGround = false;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+    }
+}
